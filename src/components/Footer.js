c8b6dc429ee3c713c1e649793d6bde29154b633b
salahkhenfer <khenfer.salah@univ-ouargla.dz>
2023-03-28 08:19:54 +0100

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,13 @@ import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 function Footer() {
   return (
     <div className='footer'>
-    <p>&copy; 2023 Salah khenfer. All rights reserved .</p>
+    <p>&copy; 2023 Salah khenfer. All rights reserved
+    <div className='icon'>
     <a href="https://www.instagram.com/bnlili_off/"> <FaInstagram className='inst' /></a> 
     <a href="https://github.com/salahkhenfer"> <FaGithub className='git' /></a> 
     <a href="https://www.linkedin.com/in/salah-eddine-khenfer-738b8a241/"> <FaLinkedin className='lin' /></a> 
+    </div>
+     </p>
     
     </div>
   )
